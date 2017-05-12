36e41a931201e7ba40693bf89ddb02ca574423c5
Jean-Erick Voigt <voigtjeanerick@gmail.com>
2017-05-12 10:20:00 -0400

    edit for the right output

---
+++ project_1.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+int main(void)
+{
+	int p_numbers = numbers;	
+	int x, y; 
+	int numbers[10][10];
+	for (x = 0; x<=11; x++){
+		for (y = 0; y<=11; y++){
+			if (x == 0{
+				if(y ==0){
+					numbers[x][y] = "*";
+				}else{
+					numbers[x][y] = 1 * y
+				}
+			}
+			else if (x != 0 && y == 0){
+				numbers[x][y] = x * 1
+			}else{
+				numbers[x][y] = x * y;			
+			}
+			printf("%d", numbers[x][y]);
+		
+		}
+		printf("\n");
+	}	
+}
+
+
