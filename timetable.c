681a5666be5196b17908f5bc747997c2561dd3ed
Jean-Erick Voigt <voigtjeanerick@gmail.com>
2017-05-12 14:59:42 -0400

    Working copy with 1 challenge. Aligned the table.

---
+++ timetable.c
@@ -0,0 +1,78 @@
+#include <stdio.h>
+
+
+
+int myatoi(char s[])
+{
+	int i = 0, n = 0;
+	while (s[i] >= '0' && s[i] <= '9'){
+		n = 10 * n + s[i++] - '0';
+	}
+	return(n);
+}
+
+int spaces(int row, int column)
+{	
+	int pad;	
+	if((row * column) < 1000){
+		pad = 4;
+	}
+	else if((row * column) < 10000){
+		pad = 5;
+	}else{
+		pad = 6;
+	}	
+	return(pad);
+}
+
+
+
+int main(int argc, char *argv[])
+{	
+	int max;	
+	int i;
+	for (i = 0; i < argc; i++){
+		//printf("This is command number and line argument: %d  %s\n", i, argv[i]);
+		if(argc > 1){		
+			if (i == 1){
+				max = myatoi(argv[i]) + 1;
+				//printf("%d", max);
+				break;
+			}
+		}else{
+			max = 11;
+		}	
+	}
+	
+	int numbers[max][max];
+	int pad_space;		
+	pad_space = spaces(max, max); 
+	char asterisk = '*';		
+	int answer;
+	int x, y;
+	printf("   > timestable \n");
+	for (x = 0; x<max; x++){
+		for (y = 0; y< max; y++){
+			if ((x==0) && (y == 0)){
+				printf("%*c", pad_space, asterisk);
+					continue;
+			}
+						
+			else if (x == 0 && y != 0){
+				answer = 1 * y;
+				numbers[x][y] = answer;
+			
+			}
+			else if (x != 0 && y == 0){
+				numbers[x][y] = x * 1;
+			}else{
+				numbers[x][y] = x * y;			
+			}
+			printf("%*d", pad_space, numbers[x][y]);
+		
+		}
+		printf("\n");
+	}	
+}
+
+
